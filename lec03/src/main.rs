a260219e1026abe8cad40263b68af313e7d1d2ce
insou22 <zac.kologlu@gmail.com>
2024-02-20 19:10:38 +1100

    lec03 longest equal run

--- lec03/src/main.rs
+++ lec03/src/main.rs
@@ -10,7 +10,66 @@ struct Student {
     // wam: Option<f64>,
 }
 
+// Zero-cost abstractions
+// - C++
+// - Rust
+// - ...
+//
+// Zero-cost meaning:
+// 1. If you don't use it, you don't "pay" for it
+// 2. If you do use it, it should be as performant as hand-written code
+
 fn main() {
+    dbg!(longest_equal_run_imperative(
+            vec![6, 9, 9, 1, 1, 2, 3],
+            vec![1, 9, 9, 1, 3, 2, 3]));
+
+    dbg!(longest_equal_run_imperative(
+            vec![],
+            vec![]));
+
+    dbg!(longest_equal_run_imperative(
+            vec![1, 2, 3],
+            vec![1]));
+
+    dbg!(longest_equal_run_imperative(
+            vec![1, 2, 3],
+            vec![5]));
+
+    dbg!(longest_equal_run_functional(
+            vec![6, 9, 9, 1, 1, 2, 3],
+            vec![1, 9, 9, 1, 3, 2, 3]));
+
+    dbg!(longest_equal_run_functional(
+            vec![],
+            vec![]));
+
+    dbg!(longest_equal_run_functional(
+            vec![1, 2, 3],
+            vec![1]));
+
+    dbg!(longest_equal_run_functional(
+            vec![1, 2, 3],
+            vec![5]));
+
+    dbg!(longest_equal_run_mixed(
+            vec![6, 9, 9, 1, 1, 2, 3],
+            vec![1, 9, 9, 1, 3, 2, 3]));
+
+    dbg!(longest_equal_run_mixed(
+            vec![],
+            vec![]));
+
+    dbg!(longest_equal_run_mixed(
+            vec![1, 2, 3],
+            vec![1]));
+
+    dbg!(longest_equal_run_mixed(
+            vec![1, 2, 3],
+            vec![5]));
+
+    if true { return }
+
     let mut my_hashmap: HashMap<Student, String> = HashMap::new();
 
     // my_hashmap.insert(
@@ -69,16 +128,90 @@ fn find_the_median(mut vec: Vec<i32>) -> Option<f64> {
     }
 }
 
+// x: [6, 9, 9, 1, 1, 2, 3]
+// y: [1, 9, 9, 1, 3, 2, 3]
+//
+// z: [(6, 1), (9, 9), (9, 9), (1, 1), (1, 3), (2, 2), (3, 3)]
+// w: [F,      T,      T,      T,      F,      T,      T]
+// w: [F, T, T, T, F, T, T]
+//
+// Output: 3
+//
+// x: []
+// y: []
+// Output: 0
+//
+// x: [1, 2, 3]
+// y: [1]
+// Output: 1
+//
+// x: [1, 2, 3]
+// y: [5]
+// Output: 0
+//
+//
+// Imperative code: Explaining how to do your task
+// Functional code: Describing how to do your task
+//
 fn longest_equal_run_imperative(x: Vec<i32>, y: Vec<i32>) -> usize {
-    todo!()
+    let shorter_len = usize::min(x.len(), y.len());
+
+    let mut longest_run = 0;
+    let mut current_run = 0;
+
+    let mut i = 0;
+    while i < shorter_len {
+        let a = x[i];
+        let b = y[i];
+
+        if a == b {
+            current_run += 1;
+
+            if current_run > longest_run {
+                longest_run = current_run;
+            }
+        } else {
+            current_run = 0;
+        }
+
+        i += 1;
+    }
+
+    longest_run
 }
 
 fn longest_equal_run_functional(x: Vec<i32>, y: Vec<i32>) -> usize {
-    todo!()
+    x.into_iter().zip(y.into_iter()) // -> Iterator<Item = (i32, i32)>
+        .map(|(a, b)| a == b)        // -> Iterator<Item = bool>
+        .fold((0, 0), |(current, longest), eq| {
+            if eq {
+                let current = current + 1;
+                let longest = usize::max(current, longest);
+
+                (current, longest)
+            } else {
+                (0, longest)
+            }
+        }).1
 }
 
 fn longest_equal_run_mixed(x: Vec<i32>, y: Vec<i32>) -> usize {
-    todo!()
+    let mut current_run = 0;
+    let mut longest_run = 0;
+
+    for (a, b) in x.into_iter().zip(y.into_iter()) {
+        if a == b {
+            current_run += 1;
+
+            if current_run > longest_run {
+                longest_run = current_run;
+            }
+        } else {
+            current_run = 0;
+        }
+    }
+
+    longest_run
 }
 
 fn word_count(string: &str) -> usize {

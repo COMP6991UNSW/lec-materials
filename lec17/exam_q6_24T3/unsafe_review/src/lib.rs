78220253c214575b0d81ed64d8493aa8865be37b
Zac Kologlu <zac.kologlu@gmail.com>
2025-04-22 22:13:13 +1000

    lec17 materials

--- lec17/exam_q6_24T3/unsafe_review/src/lib.rs
+++ lec17/exam_q6_24T3/unsafe_review/src/lib.rs
@@ -1,14 +1,16 @@
 use std::{ptr, ops::Deref};
 
 pub struct Lazy<T> {
-    value: *mut T,
+    value: *mut Option<T>,
     f: fn() -> T,
 }
 
 impl<T> Lazy<T> {
     pub fn new(f: fn() -> T) -> Lazy<T> {
+        let v = Box::new(None::<T>);
+
         Self {
-            value: ptr::null_mut(),
+            value: Box::into_raw(v),
             f,
         }
     }
@@ -20,13 +22,15 @@ impl<T> Deref for Lazy<T> {
     fn deref(&self) -> &Self::Target {
         unsafe {
             // if the value is uninitialized, then initialize it!
-            if self.value.is_null() {
+            let value = self.value.read();
+            if value.is_none() {
                 let value = (self.f)();
-                self.value.write(value);
+                self.value.write(Some(value));
             }
+            std::mem::forget(value);
 
             // the value is initialized, give out a borrow
-            &*self.value
+            (*self.value).as_ref().unwrap()
         }
     }
 }

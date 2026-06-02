b6780055123b54bae0197ff5380bf8c69276fe70
Zac Kologlu <zac.kologlu@gmail.com>
2026-06-02 21:45:10 +1000

    lec01 materials

---
+++ lec01/picture/src/main.rs
@@ -0,0 +1,10 @@
+use bmp::{Image, Pixel, px};
+
+fn main() {
+    let mut img = Image::new(256, 256);
+
+    for (x, y) in img.coordinates() {
+        img.set_pixel(x, y, px!(x, y, 200));
+    }
+    let _ = img.save("img.bmp");
+}

89ddd3f8beb94864e2ddf7fb610a86ab15f872ff
Zac Kologlu <zac.kologlu@gmail.com>
2026-06-30 20:01:35 +1000

    lec09 materials

---
+++ lec09/src/main.rs
@@ -0,0 +1,89 @@
+#![allow(unused)]
+
+use std::collections::HashSet;
+
+fn main() {
+    dbg!(smallest_list(vec![5, 0, 3]).unwrap());
+    dbg!(smallest_list([2.50, 3.14, 1.25]).expect("exactly 2 inputs given"));
+    dbg!(smallest_list(HashSet::from(['z', 'a'])).unwrap_or_default());
+
+    let my_smallest_char: fn(char, char) -> char
+        = smallest::<char>;
+    let the_char = my_smallest_char('c', 'f');
+    dbg!(the_char); // 'c'
+}
+
+fn smallest_list<I, T>(xs: I) -> Option<T>
+where
+    I: IntoIterator<Item = T>,
+    T: PartialOrd,
+{
+    let mut iter = xs.into_iter();
+    let mut smallest = iter.next()?;
+    
+    for item in iter {
+        if item < smallest {
+            smallest = item;
+        }
+    }
+
+    Some(smallest)
+}
+
+// impl trait, does it work for smallest?
+// Maybe we can use From / Into to make the interface nicer?
+
+fn smallest<T>(x: T, y: T) -> T
+where
+    T: PartialOrd,
+{
+    if x < y {
+        x
+    } else {
+        y
+    }
+}
+
+//  if PartialOrd::partial_cmp(&x, &y) == Some(Ordering::Less) {
+
+fn smallest_i32(x: i32, y: i32) -> i32 {
+    if x < y {
+        x
+    } else {
+        y
+    }
+}
+
+fn smallest_f32(x: f32, y: f32) -> f32 {
+    if x < y {
+        x
+    } else {
+        y
+    }
+}
+
+fn smallest_char(x: char, y: char) -> char {
+    if x < y {
+        x
+    } else {
+        y
+    }
+}
+
+/*
+enum Either<T, U> {
+    Left(T),
+    Right(U),
+}
+
+fn smallest<T, U>(x: T, y: U) -> Either<T, U>
+where
+    T: PartialOrd<U>,
+{
+    if x < y {
+        Either::Left(x)
+    } else {
+        Either::Right(y)
+    }
+}
+*/
